6b5cf5a3627e7578c191bdcadede6ffb50c230e7
marksman1298 <marksaid206@gmail.com>
2021-10-24 18:43:40 -0400

    Finished for distinct terms, TODO repeat for nonpositional postings

--- SubProject1.py
+++ SubProject1.py
@@ -1,5 +1,6 @@
 import os, re, json
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from typing import List, Dict
 
@@ -61,9 +62,11 @@ def sortAndRemoveDuplicates(articles: List) -> List:  #case insensitive?
     # articles = sorted(articles)   
     return sorted(list(set([termDocId for termDocId in articles])))
 
-def postingsList(articles: List) -> Dict:
+#sort list of docs in increasing order?
+def postingsList(articles: List) -> List:
     #Loop through each tuple
-    postingList = []
+    # postingList = []
+    postingList = {}
     i = 0
     while(i < (len(articles)-1)):
         token = articles[i][0]
@@ -73,10 +76,76 @@ def postingsList(articles: List) -> Dict:
             i += 1
             
         i += 1
-        postingList.append((token, ids))
-
-    with open("test2.txt", "w") as outfile:
-        json.dump(postingList, outfile)
+        # postingList.append((token, ids))
+        postingList[token] = ids
+    # with open("test2.txt", "w") as outfile:
+    #     json.dump(postingList, outfile)
     return postingList
+
+#Single word query
+def queryProcessor(postingsList: List) -> List:
+    query = input("Enter single word query: ")
+    for term in postingsList:
+        if term[0] == query:
+            return term
+    return []
+
+#Subproject 3
+#1 Implement the lossy dictionary compression techiniques of table 5.1
+def distinctLossyDictionaryCompression(postingsList: Dict):
+    unfiltered = len(postingsList)
+    #remove numbers
+    noNumbers = {}
+    for term in postingsList:
+        if not term.isdecimal():
+            noNumbers[term] = postingsList[term]
+    lowerCase = caseFolding(noNumbers)
+    stopWords30, stopWords150 = removeStopWords(lowerCase)
+    porterStemmer = stemming(stopWords150)
+    print(unfiltered)
+    print(len(noNumbers))
+    print(len(lowerCase))
+    print(len(stopWords30))
+    print(len(stopWords150))
+    print(len(porterStemmer))
+
+def caseFolding(noNumbers: Dict) -> Dict:
+    lowerCase = {}
+    for term in noNumbers:
+        if bool(re.match(r'.*[A-Z].*', term)): #check if term contains any uppercase characters
+            if term.lower() in noNumbers:
+                lowerCase[term.lower()] = list(set(noNumbers[term] + noNumbers[term.lower()]))
+            else:
+                lowerCase[term.lower()] = noNumbers[term]
+        else:
+            lowerCase[term] = noNumbers[term]
+    return lowerCase
+
+def removeStopWords(lowerCase: Dict) -> Dict :
+    stopWords30 = {}
+    stopWords150 = {}
+    nltkStopwords = stopwords.words('english')
+    for term in lowerCase:
+        if term not in nltkStopwords[:30]:
+            stopWords30[term] = lowerCase[term]
+        if term not in nltkStopwords[:150]:
+            stopWords150[term] = lowerCase[term]
+    return (stopWords30, stopWords150)
+
+def stemming(stopWords150: Dict)-> Dict: #what happens if two words stem to same thing, if they do need to add contents to 
+    porterStemmer = {}
+    ps = PorterStemmer()
+    for term in stopWords150:
+        stemmedTerm = ps.stem(term)
+        if stemmedTerm in porterStemmer:
+            porterStemmer[stemmedTerm] = list(set(porterStemmer[stemmedTerm] + stopWords150[term]))
+        else:
+            porterStemmer[stemmedTerm] = stopWords150[term]
+    return porterStemmer 
+
 articles = sortAndRemoveDuplicates(splitIntoArticles())
-postingsList(articles)
+articles = postingsList(articles)
+distinctLossyDictionaryCompression(articles)
+# print(queryProcessor(articles))
+
+
